81ddad52e73f5657910e55e57d4db03bc18868c8
ainochi-kor <kthrkdals@gmail.com>
2025-05-31 23:55:30 +0900

    feat: 풀 라우트 캐시 동적 경로에 적용하기

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -10,7 +10,10 @@ export default async function Page({
   const { q } = await searchParams;
 
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search ?q=${q}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search ?q=${q}`,
+    {
+      cache: "force-cache", // 캐시를 사용하여 검색 결과를 빠르게 가져옴
+    }
   );
 
   if (!response.ok) {
---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>죄송합니다, 요청하신 페이지를 찾을 수 없습니다.</p>
+      <p>다른 페이지로 이동해 주세요.</p>
+    </div>
+  );
+}
